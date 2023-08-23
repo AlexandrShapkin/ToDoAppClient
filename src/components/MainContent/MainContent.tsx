96f5f8ba1195c681b2a10fa7c862708bda4d4d99
AlexandrShapkin <shapkin.alexandr.sergeevich@gmail.com>
2023-08-23 14:19:18 +0700

    Fixed main content height

--- src/components/MainContent/MainContent.tsx
+++ src/components/MainContent/MainContent.tsx
@@ -4,7 +4,7 @@ import TasksList from "../UI/TasksList/TasksList";
 
 function MainContent() {
   return (
-    <main className="m-auto flex flex-col w-full md:w-[70vw] h-[95vh] sm:h-[85vh] md:h-[85vh] overflow-y-auto">
+    <main className="m-auto flex flex-col w-full md:w-[70vw] h-full overflow-auto">
       <TasksList />
     </main>
   );
--- src/components/UI/TasksList/TasksList.tsx
+++ src/components/UI/TasksList/TasksList.tsx
@@ -14,7 +14,7 @@ function TasksList() {
 
 
   return (
-    <ul className="divide-y devide-granite-gray space-y-[1rem]">
+    <ul className="divide-y devide-granite-gray space-y-[1rem] h-[90vh]">
       {tasksContext?.tasks?.map((task) => (
         <TaskElement taskData={task} setTaskDone={tasksContext?.setTaskDone} deleteTask={tasksContext?.deleteTask} updateTask={tasksContext?.updateTask} key={task._id} />
       ))}
