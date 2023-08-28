6abbe6c5d31c50a6661911ef51d3cfefc2e2e7df
AlexandrShapkin <shapkin.alexandr.sergeevich@gmail.com>
2023-08-28 14:46:15 +0700

    Simple task sortings

--- src/components/UI/TasksList/TasksList.tsx
+++ src/components/UI/TasksList/TasksList.tsx
@@ -6,6 +6,7 @@ import { UserContext, TasksContext } from "../../../App";
 
 import { AiOutlinePlus } from "react-icons/ai";
 import AddTaskModal from "../../AddTaskModal/AddTaskModal";
+import Task from "../../../types/Task";
 
 function TasksList() {
   const userContext = useContext(UserContext);
@@ -40,20 +41,36 @@ function TasksList() {
     return groups;
   };
 
+  type TaskField = keyof Task;
+  const sortFields = [
+    ["Время добавления", "addTime"],
+    ["Время выполнения", "doneTime"],
+    ["Название", "header"],
+  ];
+  const [sorter, setSorter] = useState<TaskField>(
+    sortFields[0][1] as TaskField
+  );
+
+  const changeSorterHandler = (e: ChangeEvent<HTMLSelectElement>) => {
+    setSorter(e.target.value as TaskField);
+  };
+
   return (
     <ul className="divide-y devide-granite-gray mx-auto space-y-[1rem] h-[90vh] sm:h-[80vh] md:h-[86vh] w-full md:w-[30rem]">
       <div className="flex flex-row h-[1rem] justify-between">
-        <button className="text-[2rem] ease-in-out delay-150 hover:scale-110" onClick={showModalHandler}>
+        <button
+          className="text-[2rem] ease-in-out delay-150 hover:scale-110"
+          onClick={showModalHandler}
+        >
           <AiOutlinePlus />
         </button>
         <AddTaskModal showModal={showModal} hideModal={showModalHandler} />
         <div>
           <select
             className="h-[2rem] w-[8rem] bg-transparent truncate"
-            name=""
-            id=""
+            name="filter"
+            id="filter"
             value={filter}
-            defaultValue={""}
             onChange={changeFilterHandler}
           >
             <option value={""}>{"Без фильтра"}</option>
@@ -63,31 +80,54 @@ function TasksList() {
               </option>
             ))}
           </select>
+          <select
+            className="h-[2rem] w-[11rem] bg-transparent truncate"
+            onChange={changeSorterHandler}
+          >
+            {sortFields.map((sortField) => (
+              <option key={sortField[1]} value={sortField[1]}>
+                {sortField[0]}
+              </option>
+            ))}
+          </select>
         </div>
       </div>
-      {tasksContext?.tasks?.map((task) => {
-        if (filter == "") {
-          return (
-            <TaskElement
-              taskData={task}
-              setTaskDone={tasksContext?.setTaskDone}
-              deleteTask={tasksContext?.deleteTask}
-              updateTask={tasksContext?.updateTask}
-              key={task._id}
-            />
-          );
-        } else if (task.group && filter && task.group.indexOf(filter) != -1) {
-          return (
-            <TaskElement
-              taskData={task}
-              setTaskDone={tasksContext?.setTaskDone}
-              deleteTask={tasksContext?.deleteTask}
-              updateTask={tasksContext?.updateTask}
-              key={task._id}
-            />
-          );
-        }
-      })}
+      {tasksContext?.tasks
+        ?.sort((a, b) => {
+          if (!a[sorter]) {
+            return 1;
+          } else if (!b[sorter]) {
+            return -1;
+          } else if (a[sorter]! > b[sorter]!) {
+            return 1;
+          } else if (a[sorter]! < b[sorter]!) {
+            return -1;
+          }
+          return 0;
+        })
+        .map((task) => {
+          if (filter == "") {
+            return (
+              <TaskElement
+                taskData={task}
+                setTaskDone={tasksContext?.setTaskDone}
+                deleteTask={tasksContext?.deleteTask}
+                updateTask={tasksContext?.updateTask}
+                key={task._id}
+              />
+            );
+          } else if (task.group && filter && task.group.indexOf(filter) != -1) {
+            return (
+              <TaskElement
+                taskData={task}
+                setTaskDone={tasksContext?.setTaskDone}
+                deleteTask={tasksContext?.deleteTask}
+                updateTask={tasksContext?.updateTask}
+                key={task._id}
+              />
+            );
+          }
+        })}
     </ul>
   );
 }
